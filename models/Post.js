cb86ce9f30963a6b73b87006f9cb13e0ea4b52f5
Kingluddite <howley.phil@gmail.com>
2020-06-25 14:49:44 -0400

    Add github api for getting repos and start working on posts model and routes

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String,
+      },
+      data: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  data: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('post', PostSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+
+// Bring in our models
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
 const router = express.Router();
 
-// @route    GET api/posts
-// @desc     Test route
-// @access    Public
-router.get('/', (req, res) => res.send('posts route'));
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Public
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Post content is required')
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    // error checking
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
